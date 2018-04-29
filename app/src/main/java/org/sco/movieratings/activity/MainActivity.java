22c5e4092088431d0cd6ed7887318ecb41d549b0
Scott Argenziano <scott.arg@gmail.com>
2018-04-29 12:12:51 -0400

    Remove settings activity

--- app/src/main/java/org/sco/movieratings/activity/MainActivity.java
+++ app/src/main/java/org/sco/movieratings/activity/MainActivity.java
@@ -1,13 +1,8 @@
 package org.sco.movieratings.activity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.sco.movieratings.R;
 
@@ -29,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             mSort = savedInstanceState.getString(SORT_MODE, getString(R.string.pref_sort_top_rated));
         }
-        setTitle();
     }
 
     @Override
@@ -39,56 +33,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onPause() {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-//        prefs.edit().putString(getString(R.string.pref_sort_key),mSort).apply();
-        super.onPause();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.sort_by) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
-        setTitle();
-    }
-
-    public void setTitle() {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String sortType = prefs.getString(getString(R.string.pref_sort_key),
-                getString(R.string.pref_sort_top_rated));
-
-        String title;
-        switch (sortType) {
-            case "top_rated":
-                title = getString(R.string.high_rated_settings);
-                break;
-            case "popular":
-                title = getString(R.string.most_popular_settings);
-                break;
-            case "my_favorites":
-                title = getString(R.string.my_favorites_settings);
-                break;
-            default:
-                title = "";
-                break;
-        }
-
     }
 
     public void setTitle(final String title) {
