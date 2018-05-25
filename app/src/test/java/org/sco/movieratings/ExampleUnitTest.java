9ea6d87c8dd49d889399d367ba4ffa8740155796
Scott Argenziano <sargenzi@amazon.com>
2018-05-25 08:53:54 -0400

    Added Example Junit test

---
+++ app/src/test/java/org/sco/movieratings/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package org.sco.movieratings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void sample() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
