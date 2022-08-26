30de08845b5fa7cc0a7cb016545a07dfdebd3993
Scott Argenziano <scott.arg@gmail.com>
2022-08-26 17:38:53 -0400

    Adding Color And Theming

--- app/src/main/java/org/sco/movieratings/MainActivity.kt
+++ app/src/main/java/org/sco/movieratings/MainActivity.kt
@@ -16,7 +16,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            AppTheme {
+                MainScreen()
+            }
+
         }
     }
 }
