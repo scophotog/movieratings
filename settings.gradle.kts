7932f8bca70aeaef81cb7bf55c669ef175745a5f
Scott Argenziano <sargenzi@audible.com>
2023-03-02 08:57:26 -0500

    Remove unneeded includes in main branch

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,27 +24,3 @@ dependencyResolutionManagement {
 
 include(":app")
 include(":espresso")
-include(":core:network:impl")
-include(":core:db:impl")
-include(":core:coroutine:impl")
-
-include(":feature:shared:api")
-include(":feature:shared:impl")
-include(":feature:shared:wiring")
-
-include(":feature:movie_details:api")
-include(":feature:movie_details:impl")
-include(":feature:movie_details:wiring")
-include(":feature:movie_details:fake")
-include(":feature:movie_details:fake-wiring")
-include(":feature:movie_details:ui")
-
-include(":feature:movie_list:api")
-include(":feature:movie_list:impl")
-include(":feature:movie_list:wiring")
-include(":feature:movie_list:fake")
-include(":feature:movie_list:fake-wiring")
-include(":feature:movie_list:demo")
-include(":feature:movie_list:demo-carousel")
-include(":feature:movie_list:ui")
-include(":feature:movie_list:ui2")
