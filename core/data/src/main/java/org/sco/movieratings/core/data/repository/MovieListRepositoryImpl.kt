b2d436ed6573e368c4aca8dca30a834b1709a218
Scott Argenziano <scott.arg@gmail.com>
2025-06-19 17:14:53 -0400

    Repository to use ioDispatcher

--- core/data/src/main/java/org/sco/movieratings/core/data/repository/MovieListRepositoryImpl.kt
+++ core/data/src/main/java/org/sco/movieratings/core/data/repository/MovieListRepositoryImpl.kt
@@ -1,11 +1,15 @@
 package org.sco.movieratings.core.data.repository
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import org.sco.movieratings.core.data.model.toMovieSchema
 import org.sco.movieratings.core.model.data.MovieListItem
+import org.sco.movieratings.core.network.Dispatcher
+import org.sco.movieratings.core.network.MovieRatingDispatchers
 import org.sco.movieratings.db.dao.MovieDao
 import org.sco.movieratings.db.model.toMovieListItem
 import org.sco.movieratings.network.TheMovieDbNetworkDataSource
@@ -16,7 +20,8 @@ import javax.inject.Inject
 
 class MovieListRepositoryImpl @Inject constructor(
     private val service: TheMovieDbNetworkDataSource,
-    private val movieDao: MovieDao
+    private val movieDao: MovieDao,
+    @Dispatcher(MovieRatingDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
 ) : MovieListRepository {
 
     private val popularMoviesCache: MutableList<MovieListItem> = mutableListOf()
@@ -24,51 +29,64 @@ class MovieListRepositoryImpl @Inject constructor(
     private val allMovies: List<MovieListItem>
         get() = (popularMoviesCache + topMoviesCache)
 
-    override suspend fun getPopularMovies(): Flow<List<MovieListItem>> {
-        // TODO: Implement retrieving from cache
-        service.getPopularMovies().apply {
-            this.mapTo(popularMoviesCache) { it.toMovieListItem() }
+    override suspend fun getPopularMovies(): Flow<List<MovieListItem>> =
+        withContext(ioDispatcher) {
+            // TODO: Implement retrieving from cache
+            service.getPopularMovies().apply {
+                this.mapTo(popularMoviesCache) { it.toMovieListItem() }
+            }
+            flowOf(popularMoviesCache)
         }
-        return flowOf(popularMoviesCache)
-    }
 
-    override suspend fun getTopRatedMovies(): Flow<List<MovieListItem>> {
-        // TODO: Implement retrieving from cache
-        service.getTopRatedMovies().apply {
-            this.mapTo(topMoviesCache) { it.toMovieListItem() }
+    override suspend fun getTopRatedMovies(): Flow<List<MovieListItem>> =
+        withContext(ioDispatcher) {
+            // TODO: Implement retrieving from cache
+            service.getTopRatedMovies().apply {
+                this.mapTo(topMoviesCache) { it.toMovieListItem() }
+            }
+            flowOf(topMoviesCache)
         }
-        return flowOf(topMoviesCache)
-    }
 
-    override suspend fun getMovie(movieId: Int): Flow<MovieListItem?> {
-        val movie = allMovies.find { movieId == it.id }?.apply {
-            if (this.previewList.isEmpty()) this.previewList.addAll(
-                service.getMoviePreviews(
-                    this.id
-                ).map { it.toMoviePreviewItem() })
-            if (this.reviewList.isEmpty()) this.reviewList.addAll(
-                service.getMovieReviews(this.id).map { it.toReviewItem() })
+
+    override suspend fun getMovie(movieId: Int): Flow<MovieListItem?> =
+        withContext(ioDispatcher) {
+            val movie = allMovies.find { movieId == it.id }?.apply {
+                if (this.previewList.isEmpty()) this.previewList.addAll(
+                    service.getMoviePreviews(
+                        this.id
+                    ).map { it.toMoviePreviewItem() })
+                if (this.reviewList.isEmpty()) this.reviewList.addAll(
+                    service.getMovieReviews(this.id).map { it.toReviewItem() })
+            }
+            flowOf(movie)
         }
-        return flowOf(movie)
-    }
 
-    override suspend fun isFavorite(movieId: Int): Flow<Boolean> =
+    override suspend fun isFavorite(movieId: Int): Flow<Boolean> = withContext(ioDispatcher) {
         flowOf(movieDao.findFavorite(movieId).firstOrNull() != null)
+    }
+
 
     override suspend fun getFavoriteMovies(): Flow<List<MovieListItem>> =
-        movieDao.getFavorites().map { movieItems ->
-            movieItems.map { it.toMovieListItem() }
+        withContext(ioDispatcher) {
+            movieDao.getFavorites().map { movieItems ->
+                movieItems.map { it.toMovieListItem() }
+            }
         }
 
     override suspend fun addToFavorites(movieId: Int) {
-        getMovie(movieId).collect {
-            it?.let { movie -> movieDao.addFavorite(movie.toMovieSchema())}
+        withContext(ioDispatcher) {
+            getMovie(movieId).collect {
+                it?.let { movie -> movieDao.addFavorite(movie.toMovieSchema()) }
+            }
         }
+
     }
 
     override suspend fun removeFromFavorites(movieId: Int) {
-        getMovie(movieId).collect {
-            it?.let { movie -> movieDao.removeFavorite(movie.toMovieSchema())}
+        withContext(ioDispatcher) {
+            getMovie(movieId).collect {
+                it?.let { movie -> movieDao.removeFavorite(movie.toMovieSchema()) }
+            }
         }
     }
 }
